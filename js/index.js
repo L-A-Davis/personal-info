7b3577d72faed7eae084706e06a796b98535b5fb
L-A-Davis <L-A-Davis@users.noreply.github.com>
2017-10-11 21:51:23 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+document.getElementById("integrity").addEventListener("click", function() {
+document.getElementById("integrity").innerHTML =""
+document.getElementById("integrity").innerHTML +="Integrity – Be true to myself, uphold my principles and take care of things that are important to me";
+})
+
+document.getElementById("respect").addEventListener("click", function() {
+document.getElementById("respect").innerHTML =""
+document.getElementById("respect").innerHTML +="Respect – Treat everyone with it";
+})
+
+document.getElementById("excellence").addEventListener("click", function() {
+document.getElementById("excellence").innerHTML =""
+document.getElementById("excellence").innerHTML +="Excellence – Continue to push myself to the best of my abilities";
+})
+
+document.getElementById("results").addEventListener("click", function() {
+document.getElementById("results").innerHTML =""
+document.getElementById("results").innerHTML +="Results – Achieve goals I set out to meet";
+})
+
+document.getElementById("determination").addEventListener("click", function() {
+document.getElementById("determination").innerHTML =""
+document.getElementById("determination").innerHTML +="Determination – Expect that things will be hard and plan accordingly";
+})
+
+document.getElementById("creativity").addEventListener("click", function() {
+document.getElementById("creativity").innerHTML =""
+document.getElementById("creativity").innerHTML +="Creativity – Think for myself, innovate, do not just accept things are correct just because they are in-place";
+})
+
+document.getElementById("positivity").addEventListener("click", function() {
+document.getElementById("positivity").innerHTML =""
+document.getElementById("positivity").innerHTML +="Positivity – Anything I write here is going to sound corny but the point is that nothing is ever accomplished with a poor attitude";
+})
